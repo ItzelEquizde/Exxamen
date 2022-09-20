f849de6b6b3a7ada8ccaf44132133a523eb92113
ItzelEquizde <86081169+ItzelEquizde@users.noreply.github.com>
2022-09-19 22:54:02 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <head>
+      <!-- Required meta tags -->
+    
+
+  </head>
+  <body>
+  <div>
+    <header>
+      <nav>
+          <ul class="nav justify-content-center">
+            <li><a href="#">Pizza</a></li>
+            <li><a href="#">Bebidas</a></li>
+            <li><a href="#">Hamburguesas</a></li>
+            <li><a href="#">Tacos</a></li>
+          </ul>
+        </nav>
+    </header>
+  </div>
+  <hr>
+  <div>
+    <h1>COMIDA RAPIDA</h1>
+    
+  </div>
+  <div>
+    
+      <card url="https://img.freepik.com/foto-gratis/pizza-hawaiana_1203-2455.jpg?size=626&ext=jpg&ga=GA1.2.1370031169.1661046267" nombre="Pizza hawaiana" /> 
+    
+    
+    
+    <card url="https://assets.stickpng.com/images/580b57fcd9996e24bc43c1b7.png" nombre="Hot Dogs" />
+    <card url="https://assets.stickpng.com/images/585abfc54f6ae202fedf2935.png" nombre="Papas" />
+    <Card url="https://img.freepik.com/fotos-premium/empanadas-espanolas-tipicas-blanco_123827-9710.jpg?w=826" nombre="Empanada"/>
+    <card url="https://img.freepik.com/vector-gratis/dibujado-mano-ilustracion-michelada-fresca_23-2149205755.jpg?size=338&ext=jpg&ga=GA1.2.1370031169.1661046267" nombre="Bebidas" />
+    <card url="https://i.pinimg.com/originals/ef/4a/4a/ef4a4a551b83ee26dbfe2bafe3e34699.png" nombre="Hamburguesas" />
+    <card url="https://img.freepik.com/vector-gratis/acuarela-deliciosa-ilustracion-pozole_23-2149189983.jpg?w=740&t=st=1663642195~exp=1663642795~hmac=dc2f7801238bde0bb3c4345abf8a27f58d4167640d75fdf6b1d4eabdc91e6eba" nombre="Pozole" />
+  </div>
+  <div><p>Citlally Itzel Carrera Sanchez 4°A</p></div>
+</body>
+</template>
+
+<style>
+  nav{
+ 
+
+}
+
+nav ul {
+  list-style: none;
+  overflow: hidden; /* Limpiamos errores de float */
+}
+
+nav ul li {
+  float: left;
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+}
+
+nav ul li a {
+  display: block; /* Convertimos los elementos a en elementos bloque para manipular el padding */
+  padding: 20px;
+  color: black;
+  text-decoration: none;
+}
+
+
+h1{
+  text-align: center;
+}
+body{
+  background-color: #3382FF;
+}
+
+</style>
+
+<script>
+  import Card from "./components/Card.vue"
+  
+  export default {
+    components:{
+      Card,
+    },
+  };
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+    
+    <div>
+        <img v-bind:src="url" v-bind:alt="nombre">
+        <h2>{{ nombre }}</h2>
+        <p>Descripcion del producto arre,<br>preparado con la mejor calidad</p>
+        <a v-bind:src="url" target="_blank">Comprar $150</a>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        props:{
+            url:{
+                String,
+            },
+            nombre:{
+                String,
+            },
+        },
+    };
+</script>
+
+<style scoped>
+
+    div{
+        display: inline-block;
+        text-align: center;
+        border: 15px solid #3382FF;
+    }
+    img{
+        width: 10rem;
+        padding: 2rem;
+        background: white;
+    }
+    a{
+        text-decoration: none;
+        color: snow;
+        background: #AC78CD;
+        padding: 0.3rem;
+        border-radius: 5px;
+        border: 1px solid #3382FF;
+    }
+</style>
